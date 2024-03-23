aee104def0538a52304429ee4740da3cc16459af
luojian <luojian@toukagame.com>
2024-03-23 08:29:37 +0800

    通过json配置来写sdk 版本

--- website/i18n/zh/docusaurus-plugin-content-pages/versions.js
+++ website/i18n/zh/docusaurus-plugin-content-pages/versions.js
@@ -7,145 +7,125 @@ import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 import versions from '../../../versions.json';
+import hachiSdkVersions from '../../../hachi_sdk_versions.json';
+
+function getNameFormUrl(url) {
+    // 使用URL对象来解析链接
+    const parsedUrl = new URL(url);
+// 从路径中获取文件名
+    return parsedUrl.pathname.split('/').pop();
+}
 
 function Version() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  const latestVersion = versions[0];
-  const pastVersions = versions.filter(version => version !== latestVersion);
-  const repoUrl = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}`;
-  return (
-    <Layout
-      permalink="/versions"
-      description="single-spa Versions page listing all documented site versions">
-      <div className="container margin-vert--xl">
-        <h1>HaChi SDK 更新日志</h1>
-        <div className="margin-bottom--lg">
-        <h2>最新版本</h2>
-        
-		<b><Link to={useBaseUrl('/download')}><font size="5">HaChiSDK v2.2.0</font></Link> </b>  <br/>
-        <table border="0" width="80%" align="left">
-          <tr>
-                    <td>SDK Name</td>
-                    <td>SDK Version</td>
-                    <td>Update Time</td>
-                    <td>Update Content</td>
-                    <td>Download</td>
-                  </tr>
-                  <tr>
-                    <td >HaChiSDK</td>
-                    <td>2.2.0</td>
-                    <td>2024-02-29</td>
-                    <td>
-                      1、优化adjust初始化时机<br/>
-                      2、新增firebase崩溃分析<br/>
-                      3、修复bug<br/>
-                    </td>
-                    <td>
-                    <a href="https://touka-artifacts.oss-cn-beijing.aliyuncs.com/TKG%20%E5%8F%91%E8%A1%8C%E6%8A%80%E6%9C%AF/Hachi%20SDK/Unity/2.2.0/HCSDK_2.2.0_ef0772a.unitypackage">HCSDK_2.2.0_ef0772a.unitypackage</a><br/>
-                    </td>
-          </tr>
-          </table>
-          <br/><br/><br/><br/><br/><br/>
-        </div>
+    const context = useDocusaurusContext();
+    const {siteConfig = {}} = context;
+    const latestVersion = versions[0];
+    const pastVersions = versions.filter(version => version !== latestVersion);
+    const repoUrl = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}`;
+    if (hachiSdkVersions.length < 1) {
+        return (
+            <Layout permalink="/versions"
+                    description="single-spa Versions page listing all documented site versions">
+                <div>
+                    没有配置sdk,请到配置文件中先配置 hachi_sdk_versions.json
+                </div>
+            </Layout>
+        )
+    }
 
-        <div className="margin-bottom--lg">
-                <h3 id="archive">历史版本</h3>
-                <hr />
-          <b><font size="4">HaChiSDK v2.1.0</font> </b>  <br/>
-          <table border="0" width="80%" align="left">
-            <tr>
-                      <td>SDK Name</td>
-                      <td>SDK Version</td>
-                      <td>Update Time</td>
-                      <td>Update Content</td>
-                      <td>Download</td>
-                    </tr>
-                    <tr>
-                      <td >HaChiSDK</td>
-                      <td>2.1.0</td>
-                      <td>2024-02-07</td>
-                      <td>
-                      1、修复bug
-                      </td>
-                      <td>
-                      <a href="https://touka-artifacts.oss-cn-beijing.aliyuncs.com/TKG%20%E5%8F%91%E8%A1%8C%E6%8A%80%E6%9C%AF/Hachi%20SDK/Unity/2.1.0/HCSDK_2.1.0_f0ea0b4.unitypackage">HCSDK_2.1.0_f0ea0b4.unitypackage</a><br/>
-                      </td>
-            </tr>
-            </table>
-            <br/><br/><br/><br/><br/>
-          
-              <b><font size="4">HaChiSDK v2.0.0</font> </b>  <br/>
-              <table border="0" width="80%" align="left">
-                <tr>
-                          <td>SDK Name</td>
-                          <td>SDK Version</td>
-                          <td>Update Time</td>
-                          <td>Update Content</td>
-                          <td>Download</td>
-                        </tr>
-                        <tr>
-                          <td >HaChiSDK</td>
-                          <td>2.0.0</td>
-                          <td>2024-01-05</td>
-                          <td>
-                          1、所有需要填写的ID均改为配置文件读取
-                          </td>
-                          <td>
-                          <a href="https://touka-artifacts.oss-cn-beijing.aliyuncs.com/TKG%20%E5%8F%91%E8%A1%8C%E6%8A%80%E6%9C%AF/Hachi%20SDK/Unity/2.0.0/HCSDK_2.0.0_32a0ea8.unitypackage">HCSDK_2.0.0_32a0ea8.unitypackage</a><br/>
-                          </td>
-                </tr>
-                </table>
-                <br/><br/><br/><br/><br/>
-              <b><font size="4">HaChiSDK v1.1.2</font> </b>  <br/>
-              <table border="0" width="80%" align="left">
-                <tr>
-                          <td>SDK Name</td>
-                          <td>SDK Version</td>
-                          <td>Update Time</td>
-                          <td>Update Content</td>
-                          <td>Download</td>
-                        </tr>
-                        <tr>
-                          <td >HaChiSDK</td>
-                          <td>1.1.2</td>
-                          <td>2023-11-09</td>
-                          <td>
-                          1、展示激励视频接口新增 toast 弹框参数，默认开启
-                          </td>
-                          <td>
-                          <a href="https://touka-artifacts.oss-cn-beijing.aliyuncs.com/TKG%20%E5%8F%91%E8%A1%8C%E6%8A%80%E6%9C%AF/Hachi%20SDK/Unity/1.1.2/HCSDK_1.1.2_47527f7.unitypackage">HCSDK_1.1.2_47527f7.unitypackage</a><br/>
-                          </td>
-                </tr>
-                </table>
-                <br/><br/><br/><br/><br/>
-                  <b><font size="4">HaChiSDK v1.0.0</font> </b>  <br />
-                  <table border="0" width="80%" align="left">
-                      <tr>
-                          <td>SDK Name</td>
-                          <td>SDK Version</td>
-                          <td>Update Time</td>
-                          <td>Update Content</td>
-                          <td>Download</td>
-                      </tr>
-                      <tr>
-                          <td >HaChiSDK </td>
-                          <td>1.0.0</td>
-                          <td>2023-10-26</td>
-                          <td>
-                                1、初始版本
-                          </td>
-                          <td>
-                              <a href="https://touka-artifacts.oss-cn-beijing.aliyuncs.com/TKG%20%E5%8F%91%E8%A1%8C%E6%8A%80%E6%9C%AF/Hachi%20SDK/Unity/1.0.0/HCSDK_1.0.0_dc754a8.unitypackage">HCSDK_1.0.0_dc754a8.unitypackage</a><br/>
-                                                  
-                          </td>
-                      </tr>
-                  </table>
-                  </div>
-              </div>
+    const firstSDKInfo = hachiSdkVersions[0];
+    return (
+        <Layout
+            permalink="/versions"
+            description="single-spa Versions page listing all documented site versions">
+            <div className="container margin-vert--xl">
+                <h1>HaChi SDK Release note</h1>
+                <div className="margin-bottom--lg">
+                    <h2>Latest version</h2>
 
-    </Layout>
-  );
+                    <b><Link to={useBaseUrl('/download')}><font size="5">HaChiSDK
+                        v{firstSDKInfo.sdk_version}</font></Link> </b> <br/>
+                    <table border="0" width="80%" align="left">
+                        <tbody>
+                        <tr>
+                            <td style={{"width": "15%"}}>SDK Name</td>
+                            <td style={{"width": "15%"}}>SDK Version</td>
+                            <td style={{"width": "15%"}}>Update Time</td>
+                            <td style={{"width": "40%"}}>Update Content</td>
+                            <td style={{"width": "25%"}}>Download</td>
+                        </tr>
+                        </tbody>
+                        <tbody>
+                        <tr>
+                            <td style={{"width": "15%"}}>HaChiSDK</td>
+                            <td style={{"width": "15%"}}>{firstSDKInfo.sdk_version}</td>
+                            <td style={{"width": "15%"}}>{firstSDKInfo.update_time}</td>
+                            <td style={{"width": "40%"}}>
+                                {firstSDKInfo.update_content.zh.split('\n').map((line, index) => (
+                                    <React.Fragment key={index}>
+                                        {line}
+                                        <br/>
+                                    </React.Fragment>
+                                ))}
+                            </td>
+                            <td style={{"width": "25%"}}>
+                                <a href={firstSDKInfo.download}>{getNameFormUrl(firstSDKInfo.download)}</a><br/>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <br/><br/><br/><br/><br/><br/><br/>
+                </div>
+
+                {hachiSdkVersions.length > 1 ?
+                    <div className="margin-bottom--lg">
+
+                        <h3 id="archive">Historical version</h3>
+                        <hr/>
+                        {hachiSdkVersions.map((item, index) => (
+                            index !== 0 &&
+                            <div key={index}>
+                                <div><b><font size="4">HaChiSDK v{item.sdk_version}</font></b></div>
+                                <div>
+                                    <table border="0" width="80%">
+                                        <tbody>
+                                        <tr>
+                                            <td style={{"width": "15%"}}>SDK Name</td>
+                                            <td style={{"width": "15%"}}>SDK Version</td>
+                                            <td style={{"width": "15%"}}>Update Time</td>
+                                            <td style={{"width": "40%"}}>Update Content</td>
+                                            <td style={{"width": "25%"}}>Download</td>
+                                        </tr>
+                                        </tbody>
+                                        <tbody>
+                                        <tr>
+                                            <td style={{"width": "15%"}}>HaChiSDK</td>
+                                            <td style={{"width": "15%"}}>{item.sdk_version}</td>
+                                            <td style={{"width": "15%"}}>{item.update_time}</td>
+                                            <td style={{"width": "40%"}}>
+                                                {item.update_content.zh.split('\n').map((line, index) => (
+                                                    <React.Fragment key={index}>
+                                                        {line}
+                                                        <br/>
+                                                    </React.Fragment>
+                                                ))}
+                                            </td>
+                                            <td style={{"width": "25%"}}>
+                                                <a href={item.download}>{getNameFormUrl(item.download)}</a><br/>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    : <div></div>}
+
+            </div>
+
+        </Layout>
+    );
 }
 
 export default Version;
